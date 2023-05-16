902b03585d066c557df4e53a71b40e950035460c
Het Patel <hetpatel.web@gmail.com>
2023-05-16 10:07:14 -0400

    added comment on studentList

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -1,23 +1,14 @@
-/*
- * @Het Patel
- */
 package studentlist;
 
 import java.util.Scanner;
-
-/**
- *
- * @author Het
+/*
+ * @Het Patel
  */
 public class StudentList {
 
-    /**
-     * @param args the command line arguments
-     */
+    //hello this is a comment.
     public static void main(String[] args) {
-        // TODO code application logic here
-
-        // Prompt the user for number of students in class
+        
         Student[] students = new Student[5];
 
         students[0] = new Student("001", "Het");
@@ -29,20 +20,6 @@ public class StudentList {
         for (int i = 0; i < students.length; i++) {
             System.out.println(students[i].getsID() + " " + students[i].getsName());
         }
-
-        // initilize input scanner
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter student ID to search: ");
-        String ID = input.nextLine();
-        int index = -1;
-        boolean found = false;
-        for (int i = 0; i < students.length; i++) {
-            if (ID.equals(students[i].getsID())) {
-                System.out.println("Student found: " + students[i].getsName());
-                for (int i = 0; i < students.length; i++) {
-            if (ID.equals(students[i].getsID())) {
-                index = i;
-                break;
                 
     }
 
