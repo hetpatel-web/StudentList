48d70033c7768ceec80f0544419b94cec2a740fd
Het Patel <hetpatel.web@gmail.com>
2023-05-23 09:31:02 -0400

    Update Student.java
    Added comment on Student.java

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -1,6 +1,4 @@
-/*
- * @Het Patel
- */
+
 package studentlist;
 
 /**
@@ -11,7 +9,7 @@ package studentlist;
 public class Student {
     private String sID;
     private String sName;
-
+//     this has been edited on github.com to show push and pull Magic!!
     public Student(String sID, String sName) {
         this.sID = sID;
         this.sName = sName;
